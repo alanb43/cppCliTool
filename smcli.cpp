95291861888a2901b8b7779465dd15fcf7afcbab
alanb43 <ab43work@gmail.com>
2022-01-02 02:24:47 -0500

    small weather cli tool

---
+++ smcli.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <utility>
+
+using namespace std;
+
+#define location pair<double, double>
+
+location DETROIT(42.3371482629, -83.056947069);
+location ANN_ARBOR(42.27748804120473, -83.73521358074206);
+location NEW_YORK(40.768162910467154, -73.97723644816625);
+location CHICAGO(41.878653596945725, -87.64339302779283);
+location MACOMB(42.65963610185877, -82.89557941044062);
+
+int main(int argc, char** argv) {
+    
+}
